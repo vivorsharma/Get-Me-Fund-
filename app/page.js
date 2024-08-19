42a4b515937598c556b5ad777126ba11c37d95dd
Vivor Sharma <vivorsharma2022@gmail.com>
2024-08-19 21:15:58 +0530

    changes the image and content

--- app/page.js
+++ app/page.js
@@ -4,11 +4,11 @@ export default function Home() {
   return (
     <>
       <div className="flex justify-center flex-col gap-4 items-center text-white h-[44vh] px-5 md:px-0 text-xs md:text-base">
-        <div className="font-bold flex gap-6 md:gap-20 text-3xl items-center justify-center text-xl md:text-5xl">Get Me Fund!
+        <div className="font-bold flex gap-6 md:gap-2 items-center justify-center text-3xl md:text-5xl">Get Me Fund!
           <span><img className="invertImg" src="/coin.gif" width={88} alt="" /></span>
         </div>
-        <p className="text-center md:text-left">A crowdFunding platforms for creators. get funded by your fans and followers. start Now!</p>
-        <p className="text-center md:text-left">A crowdFunding platforms for creators. get funded by your fans and followers. start Now!</p>
+        <p className="text-center md:text-left">A crowdFunding platforms for creators to fund projects</p>
+        <p className="text-center md:text-left">A place where your supporters can get you a funds. unleash the power of your supporters and get your projects funded.</p>
         <div>
           <Link href={"/login"}>
             <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 
@@ -29,18 +29,18 @@ export default function Home() {
         <h1 className="text-3xl font-bold text-center mb-14">Your fans can give you fund</h1>
         <div className="flex gap-5 justify-around">
           <div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={88} />
-            <p className="font-bold text-center">Fans want to help</p>
-            <p className="text-center">your fans are avaliable to help</p>
+            <img className="bg-slate-400 rounded-full p-2 text-black" src="/users.gif" width={88} />
+            <p className="font-bold text-center">Supporters want to help</p>
+            <p className="text-center">Your supporters are available to support you.</p>
           </div>
           <div className="item space-y-3 flex flex-col items-center justify-center">
             <img className="bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={88} />
-            <p className="font-bold text-center">Fans want to help</p>
-            <p className="text-center">your fans are avaliable to help</p>
+            <p className="font-bold text-center">Supporters want to contribute</p>
+            <p className="text-center">Your supporters are willing to contribute financially.</p>
           </div><div className="item space-y-3 flex flex-col items-center justify-center">
-            <img className="bg-slate-400 rounded-full p-2 text-black" src="/coin.gif" width={88} />
-            <p className="font-bold text-center">Fans want to help</p>
-            <p className="text-center">your fans are avaliable to help</p>
+            <img className="bg-slate-400 rounded-full p-2 text-black" src="/collaborate.gif" width={88} />
+            <p className="font-bold text-center">Supporters want to collaborate</p>
+            <p className="text-center">Your supporters want to collaborate with you.</p>
           </div>
         </div>
       </div>
